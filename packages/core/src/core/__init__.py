7e0f4d86157165ba3c65270e6e1e7ddbb6a509d8
Todd Jacobsen <yolabingo@gmail.com>
2025-05-03 13:16:53 -0600

    initial commit

---
+++ packages/core/src/core/__init__.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    print("Hello from core!")
+
+__version__ = "0.0.0"
+
+def hi() -> str:
+    return "hi from core"
---
+++ packages/svc1/src/svc1/__init__.py
@@ -0,0 +1,7 @@
+from core import hi
+
+__version__ = "0.0.0"
+
+
+def main() -> None:
+    print(hi())
